2ac3ac286789ee93138e0b76ae203749a44c6833
Stefan McShane <stefanmcshane@users.noreply.github.com>
2024-03-12 15:48:57 -0400

    update branch and add logs

--- provisioning/testdata/update-permissions.sh
+++ provisioning/testdata/update-permissions.sh
@@ -6,4 +6,6 @@ then
     exit 1
 fi
 
+echo "Running permissions update for project $PROJECT_ID"
 ccp-cli manage cloud update-permissions --project-id $PROJECT_ID
+echo "Permissions updated for project $PROJECT_ID - exit code: $?"
